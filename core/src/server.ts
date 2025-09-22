214bcd12cff0891401080329f1c5aa9bf8593efb
asafzaf <asaf.zafrir@gmail.com>
2025-09-22 10:50:40 +0300

    Fix.

--- core/src/server.ts
+++ core/src/server.ts
@@ -17,15 +17,25 @@ connectToMongo();
 
 app.use(morgan(envConfig.getEnvState() === "development" ? "dev" : "combined"));
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:3000",
+  "https://asafzaf.github.io",
+  "https://unslender-madeleine-sicklily.ngrok-free.app",
+];
+
 app.use(
   cors({
-    origin: process.env.ORIGIN || [
-      "http://localhost:5173",
-      "https://asafzaf.github.io",
-    ],
+    origin: function (origin, callback) {
+      // allow requests with no origin (like Postman or curl)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        return callback(new Error("CORS not allowed"), false);
+      }
+      return callback(null, true);
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Accept"],
-    credentials: true,
   })
 );
 app.use(express.json());
