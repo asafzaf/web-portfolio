b47b68f47a7b6dce9c258a8dfd127b25056211aa
asafzaf <asaf.zafrir@gmail.com>
2025-09-22 11:03:53 +0300

    Fix creds

--- core/src/server.ts
+++ core/src/server.ts
@@ -36,6 +36,7 @@ app.use(
     },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Accept"],
+    credentials: false,
     optionsSuccessStatus: 204,
   })
 );
